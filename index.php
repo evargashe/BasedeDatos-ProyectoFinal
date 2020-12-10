6bae0701f71d424eff410f847a2f2386fcd5d594
Edwar Vargas Herhuay <evargas@unsa.edu.pe>
2020-12-10 15:44:45 -0500

    configuracion base de datos mysql

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+require_once('config.php');
+
+$conexion = mysqli_connect('localhost:8080','root','andre123','bicicleta');
+$consulta= "SELECT * FROM producto";
+$resultado = mysqli_query($conexion,$consulta);
+while($row=mysqli_fetch_array($resultado))
+{
+  echo "id_producto: ".$row['id_producto'];
+  echo "precio: ".$row['precio'];
+}
 ?>
