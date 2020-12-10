3717331336aa7d3b8f3c4f2512b87f6e4a4e974a
Edwar Vargas Herhuay <evargas@unsa.edu.pe>
2020-12-10 15:46:06 -0500

    configuracion mysql

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once('config.php');
 
-$conexion = mysqli_connect('localhost:8080','root','andre123','bicicleta');
 $consulta= "SELECT * FROM producto";
 $resultado = mysqli_query($conexion,$consulta);
 while($row=mysqli_fetch_array($resultado))
